1a217ea59b66e3fb2bc4eab692934fd75215eea3
chapnickc <chapnickc@slu.edu>
2016-09-07 20:08:49 -0500

    Added script to run multiple jobs

--- many_job.sh
+++ many_job.sh
@@ -1,13 +1,8 @@
-
-
-if [ -f run.sh ]; then
+#!/bin/bash
+if [ -f run.sh ]                # check if job script exists in dir
+then
     for i in {1..20}
     do
-        qsub -q std ./run.sh
+        qsub -q std ./run.sh    # submit the job to the standard queue
     done
 fi
-
-
-
-
-
